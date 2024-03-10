d1519c34c828973a89d96c2c8d5b5e0d2571879a
Rachit Mittal <mittalrachit10063003@gmail.com>
2024-03-11 01:18:53 +0530

    Refactored the code

--- game_function.py
+++ game_function.py
@@ -17,6 +17,7 @@ def check_events(shipObject,bullets,settings,screen):
         else:
             pass
 
+
 def update_screen(setting,screen,ship,bullets,aliens):
     """Update the screen"""
     screen.fill(setting.bg_color)
@@ -27,6 +28,7 @@ def update_screen(setting,screen,ship,bullets,aliens):
         bullet.draw_bullet()
 
     pygame.display.flip()
+
 
 def check_keydown_events(event,shipObject,settings,screen,bullets):
     """Respond to Key Presses"""
@@ -39,12 +41,14 @@ def check_keydown_events(event,shipObject,settings,screen,bullets):
     elif event.key == pygame.K_q:
         sys.exit()
 
+
 def check_keyup_events(event,shipObject,setting,screen,bullets):
     """Respond to Key Releases"""
     if event.key == pygame.K_RIGHT:
         shipObject.moving_right = False
     elif event.key == pygame.K_LEFT:
         shipObject.moving_left = False
+
 
 def delete_bullets(bullets):
     """This function deletes the bullets when their y co-ordinate becomes less than 0.
@@ -53,23 +57,33 @@ def delete_bullets(bullets):
         if bullet.rect.bottom <= 0:
             bullets.remove(bullet)
 
+
 def fire_bullets(bullets,screen,settings,shipObject):
     new_bullet = Bullet(screen,settings,shipObject)
     bullets.add(new_bullet)
+
 
 def update_bullets_screen(bullets):
     bullets.update()
     delete_bullets(bullets)
 
+def get_number_aliens(settings,alien_width):
+    available_space_x = settings.width - 2*alien_width
+    number_aliens_x = int(available_space_x/(2*alien_width))
+    return number_aliens_x
+
+def create_alien(settings,screen,aliens,alien_number):
+    alien = Alien(settings,screen)
+    alien_width = alien.rect.width
+    alien.x = alien_width + 2*alien_width*alien_number
+    alien.rect.x = alien.x
+    aliens.add(alien)
+
 def create_fleet(settings,screen,aliens):
     """Create the full fleet of aliens."""
     alienObject = Alien(settings,screen)
     alien_width = alienObject.rect.width
-    available_space_x = settings.width - 2*alien_width
-    number_aliens_x = int(available_space_x/(2*alien_width))
+    number_aliens_x = get_number_aliens(settings,alien_width)
 
     for alien_number in range(number_aliens_x):
-        alien = Alien(settings,screen)
-        alien.x = alien_width + 2*alien_width*alien_number
-        alien.rect.x = alien.x
-        aliens.add(alien)
+        create_alien(settings,screen,aliens,alien_number)
